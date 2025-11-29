a28312ffa7b124f136099f7c0e617fc7f621861a
murilo mendo vidi <murilomendo3@gmail.com>
2025-11-29 15:39:30 -0300

    ajustes na landing page

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import {
   Button,
   IconButton,
   Image,
+  VStack,
 } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 import { CiLogin, CiUser, CiCircleQuestion } from "react-icons/ci";
@@ -21,61 +22,70 @@ export default function HomePage() {
       bgPosition="center"
       bgRepeat="no-repeat"
     >
+      {/* Header */}
       <Flex
         as="header"
         w="100%"
-        h="100px"
+        h={{ base: "70px", md: "100px" }}
         bg="#e5c4ff"
         color="black"
         align="center"
         justify="space-between"
-        px={8}
+        px={{ base: 4, md: 8 }}
         boxSizing="border-box"
         position="relative"
         zIndex={10}
       >
         <Flex align="center" gap={2}>
           <Image
-            mr="15px"
-            ml="50px"
             src="/logo.png"
             alt="logo"
-            boxSize="200px"
+            boxSize={{ base: "120px", md: "200px" }}
             objectFit="contain"
-          ></Image>
+            ml={{ base: 0, md: "50px" }}
+            mr={{ base: 0, md: "15px" }}
+          />
         </Flex>
 
-        <Flex align="center" gap={3}>
+        <Flex align="center" gap={{ base: 1, md: 3 }}>
+          {/* Desktop only */}
           <IconButton
             fontFamily="Montserrat"
             fontWeight="bold"
             bg="transparent"
             variant="ghost"
             color="#342151"
-            size="md"
+            size={{ base: "sm", md: "md" }}
             borderRadius="md"
-            px={5}
+            px={{ base: 2, md: 5 }}
+            display={{ base: "none", md: "flex" }}
             _hover={{
               bg: "#fdb525",
               color: "#342151",
               opacity: 0.9,
               transform: "scale(1.01)",
               transition: "0.3s",
+            }}
+            _active={{
+              bg: "#fdb525",
+              transform: "scale(0.98)",
             }}
             onClick={() => router.push("/")}
           >
             <CiCircleQuestion />
             Como Funciona
           </IconButton>
+
           <IconButton
             fontFamily="Montserrat"
             fontWeight="bold"
             bg="transparent"
             variant="ghost"
             color="#342151"
-            size="md"
+            size={{ base: "sm", md: "md" }}
             borderRadius="md"
-            px={5}
+            px={{ base: 2, md: 5 }}
+            display={{ base: "none", md: "flex" }}
             _hover={{
               bg: "#fdb525",
               color: "#342151",
@@ -83,13 +93,33 @@ export default function HomePage() {
               transform: "scale(1.01)",
               transition: "0.3s",
             }}
+            _active={{
+              bg: "#fdb525",
+              transform: "scale(0.98)",
+            }}
             onClick={() => router.push("/register")}
           >
             <CiUser />
             Criar Conta
-          </IconButton>  
-            
-          {/* Botão Desktop */}
+          </IconButton>
+
+          {/* Mobile */}
+          <IconButton
+            bg="transparent"
+            color="#342151"
+            size="sm"
+            minW="40px"
+            px={2}
+            display={{ base: "flex", md: "none" }}
+            _active={{
+              bg: "#fdb525",
+              transform: "scale(0.95)",
+            }}
+            onClick={() => router.push("/register")}
+          >
+            <CiUser size={24} />
+          </IconButton>
+
           <IconButton
             fontFamily="Montserrat"
             fontWeight="bold"
@@ -108,13 +138,17 @@ export default function HomePage() {
               transform: "scale(1.01)",
               transition: "0.3s",
             }}
+            _active={{
+              bg: "#fdb525",
+              transform: "scale(0.98)",
+            }}
             onClick={() => router.push("/login")}
           >
             <CiLogin />
             Entrar
           </IconButton>
 
-          {/* Botão Mobile */}
+          {/* Mobile */}
           <IconButton
             bg="transparent"
             color="#342151"
@@ -122,9 +156,9 @@ export default function HomePage() {
             minW="40px"
             px={2}
             display={{ base: "flex", md: "none" }}
-            _hover={{
+            _active={{
               bg: "#fdb525",
-              color: "#342151",
+              transform: "scale(0.95)",
             }}
             onClick={() => router.push("/login")}
           >
@@ -133,55 +167,76 @@ export default function HomePage() {
         </Flex>
       </Flex>
 
+      {/* Conteúdo Principal */}
       <Flex
         w="100%"
-        h={`calc(100vh - 70px)`}
+        minH={{ base: "calc(100vh - 70px)", md: "calc(100vh - 100px)" }}
         align="center"
-        justify="space-between"
-        px={16}
-        py={8}
+        justify={{ base: "center", md: "space-between" }}
+        px={{ base: 4, sm: 6, md: 12, lg: 16 }}
+        py={{ base: 6, md: 8 }}
+        flexDirection={{ base: "column", md: "row" }}
+        gap={{ base: 6, md: 0 }}
       >
-        <Box display="flex">
-          <Box>
-            <Heading
-              fontFamily="Montserrat"
-              as="h1"
-              fontSize="3xl"
-              color="#fff"
-              fontWeight="bold"
-              ml={55}
-              mt={-130}
-            >
-              Seu trajeto universitário, sem complicação!
-            </Heading>
-            <Button
-              fontFamily="Montserrat"
-              w="180px"
-              bg="#fdb525"
-              color="white"
-              size="lg"
-              fontSize="lg"
-              fontWeight="700"
-              borderRadius="md"
-              mt={4}
-              ml={55}
-              _hover={{
-                opacity: 0.9,
-                transform: "scale(1.01)",
-                transition: "0.3s",
-              }}
-              onClick={() => router.push("/login")}
-            >
-              Quero começar
-            </Button>
-          </Box>
-        </Box>
-        <Box display="flex" justifyContent="flex-end">
+        <VStack
+          align={{ base: "center", md: "flex-start" }}
+          spacing={{ base: 4, md: 6 }}
+          flex={1}
+          maxW={{ base: "100%", md: "600px" }}
+          ml={{ base: 0, md: 8, lg: 55 }}
+          mt={{ base: 8, md: -16, lg: -130 }}
+        >
+          <Heading
+            fontFamily="Montserrat"
+            as="h1"
+            fontSize={{ base: "2xl", sm: "3xl", md: "4xl", lg: "5xl" }}
+            color="#fff"
+            fontWeight="bold"
+            textAlign={{ base: "center", md: "left" }}
+            lineHeight="1.2"
+            px={{ base: 2, md: 0 }}
+          >
+            Seu trajeto universitário, sem complicação!
+          </Heading>
+
+          <Button
+            fontFamily="Montserrat"
+            w={{ base: "100%", sm: "220px", md: "180px" }}
+            maxW="300px"
+            bg="#fdb525"
+            color="white"
+            size={{ base: "lg", md: "lg" }}
+            fontSize={{ base: "md", md: "lg" }}
+            fontWeight="700"
+            borderRadius="md"
+            mb={{ base: 12, md: 0 }}
+            _hover={{
+              opacity: 0.9,
+              transform: "scale(1.01)",
+              transition: "0.3s",
+            }}
+            _active={{
+              transform: "scale(0.98)",
+            }}
+            onClick={() => router.push("/login")}
+          >
+            Quero começar
+          </Button>
+        </VStack>
+
+        <Box
+          display="flex"
+          justifyContent={{ base: "center", md: "flex-end" }}
+          flex={{ base: "none", md: 1 }}
+          position={{ base: "relative", md: "static" }}
+          top={{ base: "-260px", md: "auto" }}
+        >
           <Image
             src="/mascotao.png"
-            alt="Logo grande"
-            boxSize="490px"
-            mr={95}
+            alt="Mascote Unitrans"
+            boxSize={{ base: "300px", sm: "300px", md: "350px", lg: "490px" }}
+            objectFit="contain"
+            mr={{ base: 0, md: 8, lg: 95 }}
           />
         </Box>
       </Flex>
